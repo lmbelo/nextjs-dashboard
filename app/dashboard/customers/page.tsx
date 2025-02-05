d0aa799793a7224c1754f138f9ff7ba89c9ca6b3
lmbelo <lucas.belo@live.com>
2025-02-04 21:28:16 -0300

    Customers search

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,26 @@
-import { fetchFilteredCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
+import { Suspense } from "react";
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
 
-export default async function Page() {
-    const customers = await fetchFilteredCustomers('');
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+    }>
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
 
-    return <CustomersTable 
-        customers={customers}
-    />;
+    return (
+        <div className="w-full">
+            <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+                Customers
+            </h1>
+            <Search placeholder="Search customers..." />
+
+            <Suspense key={query}>
+                <CustomersTable query={query} />
+            </Suspense>
+        </div>    
+    )
 }
