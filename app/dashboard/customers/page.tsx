1a9f0a386de94f6504340cb8532f527b453db470
lmbelo <lucas.belo@live.com>
2025-02-04 21:13:06 -0300

    Costumers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+
+export default async function Page() {
+    const customers = await fetchFilteredCustomers('');
+
+    return <CustomersTable 
+        customers={customers}
+    />;
 }
